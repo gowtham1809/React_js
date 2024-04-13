ac836582e282e987c3997ee10377c2cbfd36f8dc
Gowthamraj RE <gowthamrajmamundi2002@gmail.com>
2024-04-13 21:25:32 +0530

    Update App.js

--- TIC_TAC_TOE_React_js/App.js
+++ TIC_TAC_TOE_React_js/App.js
@@ -1,1 +1,16 @@
-
+import Player from './components/Player.js';
+import GameBoard from './components/GameBoard.js';
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+      <ol id="players">
+       <Player initialName="player 1" symbol="X"/>
+       <Player initialName="player 2" symbol="O"/>
+      </ol>
+      <GameBoard/>
+      </div>
+    </main>
+  );
+}
+export default App
