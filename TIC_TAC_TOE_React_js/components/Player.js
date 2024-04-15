a18517f7daeb45b18de33b621bce1a85907f8a63
Gowthamraj RE <gowthamrajmamundi2002@gmail.com>
2024-04-15 17:54:55 +0530

    Update Player.js

--- TIC_TAC_TOE_React_js/components/Player.js
+++ TIC_TAC_TOE_React_js/components/Player.js
@@ -1,23 +1,26 @@
 import {useState} from 'react';
-export default function Player({ initialName, symbol }) {
+export default function Player({ initialName, symbol,isActive,onChangeName }) {
   const [PlayerName,setPlayerName]=useState(initialName);
   const [isEdit,setisEdit]=useState(false);
+
   function handleEdit(){
-    // setisEdit(true);
       setisEdit((isedit)=>!isedit);
+      if(isEdit){
+      onChangeName(symbol,playerName);
+      }
   }
+
   function handlechange(event){
     setPlayerName(event.target.value)
   }
+
   let playerName=<span className="player-name">{PlayerName}</span>;
   if(isEdit){
-    playerName=<input type='text' 
-    defaultValue={PlayerName}
-    onChange={handlechange}
-                 required></input>
+    playerName=<input type='text' defaultValue={PlayerName}onChange={handlechange}required></input>
   }
+
   return (
-    <li>
+    <li className={isActive ? 'active':undefined}>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
