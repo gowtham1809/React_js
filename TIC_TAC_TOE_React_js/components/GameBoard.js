8aff5630ca7f071bdc9500f78ce1b782fb5b45cb
Gowthamraj RE <gowthamrajmamundi2002@gmail.com>
2024-04-15 17:54:19 +0530

    Update GameBoard.js

--- TIC_TAC_TOE_React_js/components/GameBoard.js
+++ TIC_TAC_TOE_React_js/components/GameBoard.js
@@ -1,30 +1,18 @@
-import { useState } from "react";
-const initialGameBoard=[
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-];
-export default function GameBoard(){
-    const [gameboard,setgameboard]=useState(initialGameBoard);
-    function handleselect(rowIndex,colIndex){
-        console.log("hello1");
-        setgameboard((pregameboard)=>{
-            const updatedboard=[...pregameboard.map(innerArray=>[...innerArray])];
-            updatedboard[rowIndex][colIndex]='X';
-            console.log("hello");
-            return updatedboard;
-        })
-    }
+
+export default function GameBoard({onSelectMatrix,board}){
     return(
         <ol id="game-board">
-       {gameboard.map((row,rowIndex)=><li key={rowIndex}>
-        <ol>
-            {row.map((playerSymbol,colIndex)=>(
-            <li key={colIndex}>
-                <button onClick={()=>handleselect(rowIndex,colIndex)}>{playerSymbol}</button>
+            {board.map((row,rowIndex)=>(<li key={rowIndex}>
+                <ol>
+                    {row.map((playerSymbol,colIndex)=>(
+                    <li key={colIndex}>
+                        <button onClick={()=>onSelectMatrix(rowIndex,colIndex)} 
+                        disabled={playerSymbol!==null}>
+                        {playerSymbol}</button>
+                        
+                    </li>))}
+                </ol>
             </li>))}
-        </ol>
-       </li>)}
         </ol>
     )
 }
